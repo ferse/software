dfcc3282d5dfb55a504077acaec3b035f60d9740
Fernando Segovia <fernando170899@hotmail.com>
2022-08-12 12:30:52 -0400

    Login y Logout mejorado

--- Agile/App/views.py
+++ Agile/App/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.conf import settings
-
 
 # Create your views here.
 def home(request):
+    #Redirecciona al login si no inicio sesion y trata de entrar desde la URL
     if not request.user.is_authenticated:
-        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+        return redirect('logear')
     return render(request,"App/index.html")
 
 def logear(request):
+    #Si ya esta logeado redirecciona al inicio
+    if request.user.is_authenticated:
+        return redirect('home')
+    #Recibe los datos del formulario y almacena en las variables
     if request.method == 'POST':
         alias = request.POST['usuario']
         contra = request.POST['password']
-
+        #print(alias,contra)
         user = authenticate(username=alias, password=contra)
 
         if user is not None:
@@ -25,3 +29,7 @@ def logear(request):
             return redirect('logear')
 
     return render(request,"App/login.html")
+
+def salir(request):
+    logout(request)
+    return redirect('logear')
