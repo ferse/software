a63f60a973a7045ae75f040c490a1cbb8b68c0e3
cxrmin <110510279+cxrmin@users.noreply.github.com>
2022-08-12 19:54:26 -0400

    Primer commit

--- Agile/App/urls.py
+++ Agile/App/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'App'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='index'),
+    path('logout', views.home, name='logout'),
+    path('dashboard', views.home, name='dashboard'),
     path('logear', views.logear, name='logear'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
